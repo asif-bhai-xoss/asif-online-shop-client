18ad0c433c5d04934ae17b8dc02675e27c47bd63
asif-bhai-xoss <78955762+asif-bhai-xoss@users.noreply.github.com>
2022-11-07 11:13:36 +0600

    banner update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,32 @@
+import {
+    MDBCarousel,
+    MDBCarouselItem
+} from 'mdb-react-ui-kit';
 import React from 'react';
+import banner1 from "../../images/banner1.jpg";
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
-        </div>
+        <MDBCarousel showControls fade>
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={1}
+        src={banner1}
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={2}
+        src={banner1}
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-100 d-block'
+        itemId={3}
+        src={banner1}
+        alt='...'
+      />
+    </MDBCarousel>
     );
 };
 
